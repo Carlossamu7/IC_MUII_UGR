87a54d605ce481dfc27793c7bd952f0a30275010
Carlos <carlossamu7@gmail.com>
2020-11-11 16:25:54 +0100

    Closes #1 estructura inicial lista

--- P1-RedesNeuronales/mnist.py
+++ P1-RedesNeuronales/mnist.py
@@ -13,12 +13,12 @@
 """ Librerías """
 
 import keras
-from keras.datasets import mnist
+from keras.datasets import mnist, fashion_mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, Activation
 from keras import backend as K
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
 from tabulate import tabulate
@@ -26,24 +26,31 @@ import numpy as np
 
 """ Variables globales """
 
-batch_size = 128			# tamaño del batch
-num_classes = 10			# número de clases
-epochs = 1					# épocas
-img_rows, img_cols = 28, 28	# Dimensiones de las imágenes
+BATCH_SIZE = 128			# tamaño del batch
+N_CLASSES = 10				# número de clases
+EPOCHS = 12					# épocas
+IMG_ROWS, IMG_COLS = 28, 28	# Dimensiones de las imágenes
+SHOW_IMGS = False			# Indica si se quiere imprimir algunas imágenes
+SHOW_CONFUSSION = False		# Indica si se quiere imprimir algunas imágenes
 
 """ Uso la notación Snake Case la cual es habitual en Python """
 
-def read_data():
+def read_data(fashion=False):
 	# divide los datos en train y test
-	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	if(fashion):
+		(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+	else:
+		(x_train, y_train), (x_test, y_test) = mnist.load_data()
 	return (x_train, y_train), (x_test, y_test)
 
-def show_data(x_train):
+def show_data(x, set_init=0):
 	for i in range(9):	# sólo imprimo las primeras
-		pyplot.subplot(330 + 1 + i)
-		pyplot.imshow(x_train[i], cmap=pyplot.get_cmap('gray'))
+		plt.subplot(330 + 1 + i)
+		plt.imshow(x[i+set_init], cmap=plt.get_cmap('gray'))
 	# pintamos la imagen
-	pyplot.show()
+	plt.title("MNIST")
+	plt.gcf().canvas.set_window_title('IC - Práctica 1')
+	plt.show()
 
 def preprocess_data(x_train, y_train, x_test, y_test):
 	x_train = x_train.astype('float32')	# conversión a float32
@@ -68,8 +75,49 @@ def construc_model(input_shape):
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu'))
 	model.add(Dropout(0.5))
-	model.add(Dense(num_classes, activation='softmax'))
+	model.add(Dense(N_CLASSES, activation='softmax'))
+	model.compile(loss=keras.losses.categorical_crossentropy,
+	              optimizer=keras.optimizers.Adam(),
+	              metrics=['accuracy'])
+	return model
 
+def construc_model2(input_shape):
+	model = Sequential()
+
+	model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+	model.add(Conv2D(64, (3,3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+	model.add(Flatten())
+	model.add(Dense(64))
+
+	model.add(Dense(10))
+	model.add(Activation('softmax'))
+
+	model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+	return model
+
+def construc_model3(input_shape):
+	model = Sequential()
+
+	model.add(Conv2D(32, kernel_size=(5, 5),
+	                 activation='relu',
+	                 input_shape=input_shape))
+
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(512, activation='relu'))
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(50, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(N_CLASSES, activation='softmax'))
 	model.compile(loss=keras.losses.categorical_crossentropy,
 	              optimizer=keras.optimizers.Adam(),
 	              metrics=['accuracy'])
@@ -77,8 +125,8 @@ def construc_model(input_shape):
 
 def train_model(model, x_train, y_train, x_test, y_test):
 	model.fit(x_train, y_train,
-	          batch_size=batch_size,
-	          epochs=epochs,
+	          batch_size=BATCH_SIZE,
+	          epochs=EPOCHS,
 	          verbose=1,
 	          validation_data=(x_test, y_test))
 
@@ -88,15 +136,49 @@ def evaluate(model, x_train, y_train, x_test, y_test):
 def print_score(score_train, score_test):
 	print("\n----------  RESULTADOS  ----------")
 	# Sobre el conjunto de entrenamiento
-	print('Train loss:', score_train[0])
-	print('Train accuracy:', score_train[1])
+	print("Train loss: {:.6f}".format(score_train[0]))
+	print("Train accuracy: {:.6f}".format(score_train[1]))
 	# Sobre el conjunto de test
-	print('Test loss:', score_test[0])
-	print('Test accuracy:', score_test[1])
+	print("Test loss: {:.6f}".format(score_test[0]))
+	print("Test accuracy: {:.6f}".format(score_test[1]))
 	print("")
-	table = [["Train", score_train[1], score_train[0]],["Test", score_test[1], score_test[0]]]
+	table = [["Train", "{:.6f} %".format(100*score_train[1]), "{:.6f} %".format(100*score_train[0])],
+			 ["Test", "{:.6f} %".format(100*score_test[1]), "{:.6f} %".format(100*score_test[0])]]
 	print(tabulate(table, headers=["Conjunto", "Accuracy", "Loss"], tablefmt='fancy_grid'))
 	print("")
+
+def inverse_categorical(y_categorical):
+	y = []
+	for i in range(len(y_categorical)):
+		y.append(np.argmax(np.round(y_categorical[i])))
+	return y
+
+""" Muestra matriz de confusión.
+- y_real: etiquetas reales.
+- y_pred: etiquetas predichas.
+- message: mensaje que complementa la matriz de confusión.
+- norm (op): indica si normalizar (dar en %) la matriz de confusión. Por defecto 'True'.
+"""
+def show_confussion_matrix(y_real, y_pred, message="", norm=True):
+	mat = confusion_matrix(y_real, y_pred)
+	if(norm):
+		mat = 100*mat.astype("float64")/mat.sum(axis=1)[:, np.newaxis]
+	fig, ax = plt.subplots()
+	ax.matshow(mat, cmap="GnBu")
+	ax.set(title="Matriz de confusión {}".format(message),
+		   xticks=np.arange(10), yticks=np.arange(10),
+		   xlabel="Etiqueta", ylabel="Predicción")
+
+	for i in range(10):
+		for j in range(10):
+			if(norm):
+				ax.text(j, i, "{:.0f}%".format(mat[i, j]), ha="center", va="center",
+					color="black" if mat[i, j] < 50 else "white")
+			else:
+				ax.text(j, i, "{:.0f}".format(mat[i, j]), ha="center", va="center",
+					color="black" if mat[i, j] < 50 else "white")
+	plt.gcf().canvas.set_window_title("Práctica 3 - Clasificación")
+	plt.show()
 
 ########################
 #####     MAIN     #####
@@ -108,42 +190,54 @@ def main():
 	print("###  PRÁCTICA 1 IC - Redes Neuronales: MNIST  ###")
 	print("#################################################\n")
 
-	# Leyendo datos de entrenamiento y test
+	# Lectura de datos de entrenamiento y test
 	(x_train, y_train), (x_test, y_test) = read_data()
 	summarize_dataset(x_train, y_train, x_test, y_test)
+	x_test_orig = x_test.copy()
+	if(SHOW_IMGS):
+		show_data(x_train)
 
-	#show_data(x_train)
-
+	# Preprocesamiento
 	if K.image_data_format() == 'channels_first':
-	    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-	    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-	    input_shape = (1, img_rows, img_cols)
+	    x_train = x_train.reshape(x_train.shape[0], 1, IMG_ROWS, IMG_COLS)
+	    x_test = x_test.reshape(x_test.shape[0], 1, IMG_ROWS, IMG_COLS)
+	    input_shape = (1, IMG_ROWS, IMG_COLS)
 	else:
-	    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-	    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-	    input_shape = (img_rows, img_cols, 1)
+	    x_train = x_train.reshape(x_train.shape[0], IMG_ROWS, IMG_COLS, 1)
+	    x_test = x_test.reshape(x_test.shape[0], IMG_ROWS, IMG_COLS, 1)
+	    input_shape = (IMG_ROWS, IMG_COLS, 1)
 
 	preprocess_data(x_train, y_train, x_test, y_test)
-	# vector de etiquetas convertido a una matriz binaria
-	y_train_categorical = keras.utils.to_categorical(y_train, num_classes)
-	y_test_categorical = keras.utils.to_categorical(y_test, num_classes)
 
-	model = construc_model(input_shape)
+	# Convirtiendo el vector de etiquetas a una matriz binaria
+	y_train_categorical = keras.utils.to_categorical(y_train, N_CLASSES)
+	y_test_categorical = keras.utils.to_categorical(y_test, N_CLASSES)
+
+	# Construcción del modelo e información de las capas
+	model = construc_model3(input_shape)
+	model.summary()
+
+	# Entrenamiento del modelo
 	train_model(model, x_train, y_train_categorical, x_test, y_test_categorical)
+
+	# Evaluación del modelo
 	score_train, score_test = evaluate(model, x_train, y_train_categorical, x_test, y_test_categorical)
 	print_score(score_train, score_test)
-	x_test *= 255
-	show_data(x_test.astype('uint8'))
-	y_pred = model.predict(x_test)
-	print(y_pred)
-	print(np.argmax(np.round(y_pred)))
-	y_pred = np.argmax(np.round(y_pred))
 
-	cm = confusion_matrix(y_test, y_pred)
-	print(cm)
-	plot_confusion_matrix(model, x_test, y_test,
-                                 cmap=plt.cm.Blues)
-	pyplot.show()
+	# Imprimiendo algunos datos
+	if(SHOW_IMGS):
+		show_data(x_test_orig)
+
+	# Predicciones
+	y_pred_categorical = model.predict(x_test)
+	y_pred = inverse_categorical(y_pred_categorical)
+	print("Etiquetas predecidas: ", np.array(y_pred))
+
+	# Matriz de confusión
+	print(confusion_matrix(y_test, y_pred))
+	if(SHOW_CONFUSSION):
+		show_confussion_matrix(y_test, y_pred, "sin normalizar", False)
+		show_confussion_matrix(y_test, y_pred, "normalizada")
 
 if __name__ == "__main__":
 	main()
